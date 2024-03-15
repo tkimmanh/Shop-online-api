fcc52662222544b86e3cae07f098044c6e2542e6
kimmanh <tkmanh110329@gmail.com>
2024-03-15 23:57:03 +0700

    feat/calculate annual revenue

--- src/routes/orders.routes.js
+++ src/routes/orders.routes.js
@@ -1,13 +1,15 @@
 import { Router } from 'express'
 import {
+  calculateAnnualRevenueController,
   cancelOrderController,
   deleteOrderController,
   getAllOrdersForAdminController,
   listUserOrdersController,
-  placeOrderController
+  placeOrderController,
+  updateOrderStatusByAdminController
 } from '~/controllers/orders.controller'
 
-import { authenticateToken, isAdmin } from '~/middlewares/auth.middlewares'
+import { authenticateToken, isStaff } from '~/middlewares/auth.middlewares'
 
 const routerOrder = Router()
 
@@ -15,6 +17,7 @@ routerOrder.post('/', authenticateToken, placeOrderController)
 routerOrder.get('/my-order', authenticateToken, listUserOrdersController)
 routerOrder.patch('/:id/cancel', authenticateToken, cancelOrderController)
 routerOrder.delete('/:id/delete', authenticateToken, deleteOrderController)
-routerOrder.get('/list-orders', authenticateToken, isAdmin, getAllOrdersForAdminController)
-routerOrder.get('/:id/update-status-order', authenticateToken, isAdmin, getAllOrdersForAdminController)
+routerOrder.get('/list-orders', authenticateToken, isStaff, getAllOrdersForAdminController)
+routerOrder.patch('/:id/update-status-order', authenticateToken, isStaff, updateOrderStatusByAdminController)
+routerOrder.get('/revenue/monthly', authenticateToken, isStaff, calculateAnnualRevenueController)
 export default routerOrder
