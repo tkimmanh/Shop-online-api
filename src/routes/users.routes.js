c9899536fa02030588d7a10334da73cd0dd9a8a4
kimmanh <tkmanh110329@gmail.com>
2024-04-09 11:38:57 +0700

    feat/crud user admin

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -3,6 +3,9 @@ import {
   addToCartController,
   createUserController,
   deleteItemFromCartController,
+  deleteUserByAdminController,
+  detailUserController,
+  editUserByAdminController,
   getAllUserEmailsController,
   getCurrentUserController,
   oauthGoogleController,
@@ -11,7 +14,7 @@ import {
   updateCartItemController,
   updateUserController
 } from '~/controllers/users.controller'
-import { authenticateToken, isStaff } from '~/middlewares/auth.middlewares'
+import { authenticateToken, isAdmin, isStaff } from '~/middlewares/auth.middlewares'
 
 const routerUsers = Router()
 
@@ -25,4 +28,8 @@ routerUsers.post('/delete-cart', authenticateToken, deleteItemFromCartController
 routerUsers.post('/update-cart', authenticateToken, updateCartItemController)
 routerUsers.get('/', authenticateToken, getCurrentUserController)
 routerUsers.post('/send-email-to-all', authenticateToken, isStaff, sendEmailToAllUsersController)
+routerUsers.put('/edit-by-admin/:id', authenticateToken, isAdmin, editUserByAdminController)
+routerUsers.get('/get-all-by-admin', authenticateToken, isAdmin, editUserByAdminController)
+routerUsers.delete('/delete-by-admin/:id', authenticateToken, isAdmin, deleteUserByAdminController)
+routerUsers.get('/get-by-admin/:id', authenticateToken, isAdmin, detailUserController)
 export default routerUsers
