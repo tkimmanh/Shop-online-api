19b6270e01005a138fdb20d9336e2c8524e4a681
tkimmanh <tkmanh110329@gmail.com>
2024-05-17 23:57:12 +0700

    fix:role get all counpon

--- src/routes/coupons.routes.js
+++ src/routes/coupons.routes.js
@@ -6,12 +6,12 @@ const {
   deleteCouponController,
   getCouponController
 } = require('~/controllers/coupon.controller')
-const { isAdmin, authenticateToken } = require('~/middlewares/auth.middlewares')
+const { isAdmin, authenticateToken, isStaff } = require('~/middlewares/auth.middlewares')
 
 const couponRouter = Router()
 
 couponRouter.post('/create', authenticateToken, isAdmin, createCouponController)
-couponRouter.get('/', authenticateToken, isAdmin, listCounponController)
+couponRouter.get('/', authenticateToken, isStaff, listCounponController)
 couponRouter.get('/:id', authenticateToken, isAdmin, getCouponController)
 couponRouter.put('/:id', authenticateToken, isAdmin, updateCouponController)
 couponRouter.delete('/:id', authenticateToken, isAdmin, deleteCouponController)
